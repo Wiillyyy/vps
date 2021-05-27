e32c2ebbdec24ed956ebca4e26a75bc6a88f4b11
Wiillyyy <luigi385@gmail.com>
2021-05-27 02:45:21 +0200

    Added Website

---
+++ webpanel/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html> 
+<html lang="fr-FR">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+		<meta charset="UTF-8">
+		<title>WebPanel</title>
+		<link href="https://fonts.googleapis.com/css?family=Archivo+Black&display=swap" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		<link rel="stylesheet" type="text/css" href="assets/fontawsome/css/all.css" />
+		<script src="java.js"></script>
+        <link rel="icon" href="/assets/img/server.png" />
+	    <meta property="og:title" content="AdminPannel" />
+	    <meta property="og:description" content="La remote pour contrôler mon Serv, allumer, restart, lancer des programmes ! [EN CONSTRUCTION]" />
+	    <meta property="og:locale" content="fr_FR" />
+	    <meta property="og:image" content="https://www.apple.com/ac/structured-data/images/open_graph_logo.png?201809210816" />
+	    <meta property="og:type" content="website" />
+	    <meta property="og:site_name" content="Willy" />
+	</head>
+	<body>
+    <i class="icono-arrow4-right"></i>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-2"></div>
+            <div class="col-lg-6 col-md-8 login-box">
+                <div class="col-lg-12 login-key">
+                    <i class="fa fa-key" aria-hidden="true"></i>
+                </div>
+                <div class="col-lg-12 login-title">
+                    ADMIN PANEL
+                </div>
+
+                <div class="col-lg-12 login-form">
+                    <div class="col-lg-12 login-form">
+                        <form>
+                            <div class="form-group">
+                                <label class="form-control-label">Login</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-control-label">Mot 2 Pass</label>
+                                <input type="password" class="form-control" i>
+                                <button class="unmask" type="button" title="Mask/Unmask password to check content">Afficher M2P</button>
+                            </div>
+
+                            <div class="col-lg-12 loginbttm">
+                                <div class="col-lg-6 login-btm login-text">
+                                    <!-- Error Message -->
+                                </div>
+                                <div class="col-lg-6 login-btm login-button">
+                                    <button type="submit" class="btn btn-outline-primary">LOGIN</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-2"></div>
+            </div>
+        </div>
+        <div class="constr">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/C%C3%B4ne_orange_-_under_construction.png/526px-C%C3%B4ne_orange_-_under_construction.png" style="width:20%;">
+
+
+
+
+
